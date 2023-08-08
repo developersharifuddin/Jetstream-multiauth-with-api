1f1354a485d6243a2a1a662cbd973f93adef0a5c
Sharif uddin <sharif.uddin.9977@gmail.com>
2023-08-08 10:16:57 +0600

    button style

--- resources/views/admin/category/category.blade.php
+++ resources/views/admin/category/category.blade.php
@@ -1,157 +1,167 @@
 @extends('layouts.admin')
 @section('title', 'Category')
 @section('breadcrumb')
-<li class="breadcrumb-item"><a href="#">Home</a></li>
-<li class="breadcrumb-item active">DataTables</li>
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active">DataTables</li>
 @endsection
 @section('button') @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">DataTable with default features</h3>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">DataTable with default features</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="example1" class="table">
+                        <thead>
+                            <tr>
+                                <th>Rendering engine</th>
+                                <th>Browser</th>
+                                <th>Platform(s)</th>
+                                <th>Engine version</th>
+                                <th>CSS grade</th>
+                            </tr>
+                        </thead>
+                        <tbody id="datatable">
+                            <tr>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 4.0
+                                </td>
+                                <td>Win 95+</td>
+                                <td> 4</td>
+                                <td>
+                                    <a link="#" class="btn btn-primary btn-sm edit text-light border-0 edit"
+                                        rel="tooltip" id="edit" title="Edit" data-id="4" data-bs-toggle="modal"
+                                        data-bs-target="#edit_modal" data-bs-whatever="@mdo">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </a>
+                                    <a href="" class="btn btn-info btn-sm text-light view border-0 view"
+                                        id="view" rel="tooltip" title="view" data-id="4" data-bs-toggle="modal"
+                                        data-bs-target="#viewModal"> <i class="fas fa-eye"></i>
+                                    </a>
+                                    <a link="#" data-id="4" class="btn btn-sm btn-danger shadow border-0 delete"
+                                        id="delete">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 5.0
+                                </td>
+                                <td>Win 95+</td>
+                                <td>5</td>
+                                <td>
+                                    <a link="#" class="btn btn-primary btn-sm edit text-light border-0 edit"
+                                        rel="tooltip" id="edit" title="Edit" data-id="4" data-bs-toggle="modal"
+                                        data-bs-target="#edit_modal" data-bs-whatever="@mdo">
+                                        <i class="fas fa-pencil-alt"></i>
+                                    </a>
+                                    <a href="" class="btn btn-info btn-sm text-light view border-0 view"
+                                        id="view" rel="tooltip" title="view" data-id="4" data-bs-toggle="modal"
+                                        data-bs-target="#viewModal"> <i class="fas fa-eye"></i>
+                                    </a>
+                                    <a link="#" data-id="4" class="btn btn-sm btn-danger shadow border-0 delete"
+                                        id="delete">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 5.5
+                                </td>
+                                <td>Win 95+</td>
+                                <td>5.5</td>
+                                <td>A</td>
+                            </tr>
+
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Camino 1.5</td>
+                                <td>OSX.3+</td>
+                                <td>1.8</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Netscape 7.2</td>
+                                <td>Win 95+ / Mac OS 8.6-9.2</td>
+                                <td>1.7</td>
+                                <td>A</td>
+                            </tr>
+
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Mozilla 1.2</td>
+                                <td>Win 95+ / OSX.1+</td>
+                                <td>1.2</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Mozilla 1.3</td>
+                                <td>Win 95+ / OSX.1+</td>
+                                <td>1.3</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Mozilla 1.4</td>
+                                <td>Win 95+ / OSX.1+</td>
+                                <td>1.4</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Mozilla 1.5</td>
+                                <td>Win 95+ / OSX.1+</td>
+                                <td>1.5</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Mozilla 1.6</td>
+                                <td>Win 95+ / OSX.1+</td>
+                                <td>1.6</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Mozilla 1.7</td>
+                                <td>Win 98+ / OSX.1+</td>
+                                <td>1.7</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Mozilla 1.8</td>
+                                <td>Win 98+ / OSX.1+</td>
+                                <td>1.8</td>
+                                <td>A</td>
+                            </tr>
+                            <tr>
+                                <td>Gecko</td>
+                                <td>Seamonkey 1.1</td>
+                                <td>Win 98+ / OSX.2+</td>
+                                <td>1.8</td>
+                                <td>A</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
             </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-                <table id="example1" class="table">
-                    <thead>
-                        <tr>
-                            <th>Rendering engine</th>
-                            <th>Browser</th>
-                            <th>Platform(s)</th>
-                            <th>Engine version</th>
-                            <th>CSS grade</th>
-                        </tr>
-                    </thead>
-                    <tbody id="datatable">
-                        <tr>
-                            <td>Trident</td>
-                            <td>Internet
-                                Explorer 4.0
-                            </td>
-                            <td>Win 95+</td>
-                            <td> 4</td>
-                            <td>
-                                <a link="#" class="btn btn-info btn-sm edit text-light border-0" rel="tooltip" id="edit" title="Edit" data-id="4" data-bs-toggle="modal" data-bs-target="#edit_modal" data-bs-whatever="@mdo">
-                                    <i class="fas fa-pencil-alt"></i>
-                                </a>
-                                <a href="" class="btn btn-primary btn-sm text-light view border-0" id="view" rel="tooltip" title="view" data-id="4" data-bs-toggle="modal" data-bs-target="#viewModal"> <i class="fas fa-eye"></i>
-                                </a>
-                                <a link="#" data-id="4" class="btn btn-sm shadow border-0 delete" id="delete">
-                                    <i class="fas fa-trash"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Trident</td>
-                            <td>Internet
-                                Explorer 5.0
-                            </td>
-                            <td>Win 95+</td>
-                            <td>5</td>
-                            <td>
-                                <a link="#" class="btn btn-info btn-sm edit text-light border-0" rel="tooltip" id="edit" title="Edit" data-id="4" data-bs-toggle="modal" data-bs-target="#edit_modal" data-bs-whatever="@mdo">
-                                    <i class="fas fa-pencil-alt"></i>
-                                </a>
-                                <a href="" class="btn btn-primary btn-sm text-light view border-0" id="view" rel="tooltip" title="view" data-id="4" data-bs-toggle="modal" data-bs-target="#viewModal"> <i class="fas fa-eye"></i>
-                                </a>
-                                <a link="#" data-id="4" class="btn btn-sm shadow border-0 delete" id="delete">
-                                    <i class="fas fa-trash"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Trident</td>
-                            <td>Internet
-                                Explorer 5.5
-                            </td>
-                            <td>Win 95+</td>
-                            <td>5.5</td>
-                            <td>A</td>
-                        </tr>
-
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Camino 1.5</td>
-                            <td>OSX.3+</td>
-                            <td>1.8</td>
-                            <td>A</td>
-                        </tr>
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Netscape 7.2</td>
-                            <td>Win 95+ / Mac OS 8.6-9.2</td>
-                            <td>1.7</td>
-                            <td>A</td>
-                        </tr>
-
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Mozilla 1.2</td>
-                            <td>Win 95+ / OSX.1+</td>
-                            <td>1.2</td>
-                            <td>A</td>
-                        </tr>
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Mozilla 1.3</td>
-                            <td>Win 95+ / OSX.1+</td>
-                            <td>1.3</td>
-                            <td>A</td>
-                        </tr>
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Mozilla 1.4</td>
-                            <td>Win 95+ / OSX.1+</td>
-                            <td>1.4</td>
-                            <td>A</td>
-                        </tr>
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Mozilla 1.5</td>
-                            <td>Win 95+ / OSX.1+</td>
-                            <td>1.5</td>
-                            <td>A</td>
-                        </tr>
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Mozilla 1.6</td>
-                            <td>Win 95+ / OSX.1+</td>
-                            <td>1.6</td>
-                            <td>A</td>
-                        </tr>
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Mozilla 1.7</td>
-                            <td>Win 98+ / OSX.1+</td>
-                            <td>1.7</td>
-                            <td>A</td>
-                        </tr>
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Mozilla 1.8</td>
-                            <td>Win 98+ / OSX.1+</td>
-                            <td>1.8</td>
-                            <td>A</td>
-                        </tr>
-                        <tr>
-                            <td>Gecko</td>
-                            <td>Seamonkey 1.1</td>
-                            <td>Win 98+ / OSX.2+</td>
-                            <td>1.8</td>
-                            <td>A</td>
-                        </tr>
-
-                    </tbody>
-                </table>
-            </div>
-            <!-- /.card-body -->
+            <!-- /.card -->
         </div>
-        <!-- /.card -->
+        <!-- /.col -->
     </div>
-    <!-- /.col -->
-</div>
-<!-- /.content -->
+    <!-- /.content -->
 @endsection
